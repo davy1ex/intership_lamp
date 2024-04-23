4b5a74854c4f7789e1d45b5098bc8e6161b2a4e2
davy1ex <davy1ex@yandex.ru>
2024-04-23 20:52:12 +0400

    backup before refactoring path of docker in folder

--- src/index.php
+++ src/index.php
@@ -1,14 +1,16 @@
 <?php
 $url = trim($_SERVER["REQUEST_URI"], "/");
 
-switch ($url) {
-    case 'l-a' :
-        header("Location: /la/index.php");
-        break;
-    
-    case 'nsk' :
-        header("Location: /nsk/index.php");
-        break;
-
-}
+echo phpinfo();
+//
+//switch ($url) {
+//    case 'l-a' :
+//        header("Location: /la/index.php");
+//        break;
+//
+//    case 'nsk' :
+//        header("Location: /nsk/index.php");
+//        break;
+//
+//}
 
