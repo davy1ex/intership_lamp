7e6ef5572af57349b9cdc4ba3100bc29392ca981
davy1ex <davy1ex@yandex.ru>
2024-04-24 11:15:58 +0400

    added xdebug supported

--- src/index.php
+++ src/index.php
@@ -1,16 +1,16 @@
 <?php
 $url = trim($_SERVER["REQUEST_URI"], "/");
 
-echo phpinfo();
-//
-//switch ($url) {
-//    case 'l-a' :
-//        header("Location: /la/index.php");
-//        break;
-//
-//    case 'nsk' :
-//        header("Location: /nsk/index.php");
-//        break;
-//
-//}
+print_r($_SERVER);
+
+switch ($url) {
+    case 'l-a' :
+        header("Location: /la/index.php");
+        break;
+
+    case 'nsk' :
+        header("Location: /nsk/index.php");
+        break;
+
+}
 
